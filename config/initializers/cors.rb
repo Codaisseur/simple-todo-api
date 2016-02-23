4732e540b1f8e7b61cbe17ea43b5513521ca06c8
Wouter de Vos <wouter@codaisseur.com>
2016-02-23 10:46:07 +0100

    Debug CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,5 @@
 class TodoApi::Application
-  config.middleware.insert_before 0,  "Rack::Cors", debug: Rails.env.development?, logger: (-> { Rails.logger }) do
+  config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
       origins 'localhost:3000', '127.0.0.1:3000',
         /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
