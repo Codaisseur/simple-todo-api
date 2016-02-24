30ea1165c337b2c440d332f8877ca066eb2e90e1
Wouter de Vos <wouter@codaisseur.com>
2016-02-24 20:32:41 +0100

    Add bitballoon project to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ class TodoApi::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
       origins 'localhost:3000', '127.0.0.1:3000',
-        /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+        'project-react.bitballoon.com'
 
       resource '/cors',
         headers: :any,
